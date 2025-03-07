980c2efec18317d7e996214e6c0cbe5d8134623c
Gitcoins <gitcoins@outlook.com>
2025-03-07 08:42:31 +0000

    add llm

--- llm.py
+++ llm.py
@@ -1,6 +1,24 @@
 import streamlit as st
 
-# Create the LLM
+# Get credentails
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+openai_model = st.secrets["OPENAI_MODEL"]
 
+# Create the LLM
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(
+    openai_api_key=st.secrets["OPENAI_API_KEY"],
+    model=st.secrets["OPENAI_MODEL"],
+)
+
+
+# Create DeepSeek LLM
+from langchain.chat_models import ChatOpenAI
 
 # Create the Embedding model
+from langchain_openai import OpenAIEmbeddings
+
+embeddings = OpenAIEmbeddings(
+    openai_api_key=st.secrets["OPENAI_API_KEY"]
+)
